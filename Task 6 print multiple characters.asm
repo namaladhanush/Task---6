0312b57f2880803dc6339bede5283acc39bd839f
namaladhanush <dhanush_namala@srmap.edu.in>
2024-09-25 20:51:48 +0530

    Task 6 print multiple characters.asm

--- Task 6 print multiple characters.asm
+++ Task 6 print multiple characters.asm
@@ -1,9 +1,28 @@
-org 100h 
-jmp start 
-msg: db "Computer Organization and Architecture Task 6", 0Dh, 0Ah, 24h 
-start: mov dx, msg   
-mov ah, 09h   
-int 21h   
-mov ah, 0   
-int 16h   
-ret
+org 100h
+
+mov dx, offset msg_input
+mov ah, 09h
+int 21h
+
+mov dx, offset buffer
+mov ah, 0Ah
+int 21h
+
+mov dx, offset msg_output
+mov ah, 09h
+int 21h
+
+mov dx, offset buffer+2
+mov ah, 09h
+int 21h
+
+mov ah, 4ch
+int 21h
+
+msg_input db 'Enter a String : $'
+msg_output db 0dh, 0ah, 'The Entered String is: $'  
+buffer db 255
+       db ?
+       db 255 dup('$')
+
+end
